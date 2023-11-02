c7634697ee0c0db85843302c403255fc1cf1c3e8
aidancook89 <cookar4@appstate.edu>
2023-11-02 16:47:25 -0400

    Working on implementing AI requests for entities.

--- app/src/main/java/aidp/EntityFactory.java
+++ app/src/main/java/aidp/EntityFactory.java
@@ -2,13 +2,33 @@ package aidp;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class EntityFactory {
 
+    private static String requestJson = "{"
+    + "type: ,"
+    + "name: ,"
+    + "color: ,"
+    + "modifiers: [],"
+    + "potion_effects: [],"
+    + "generic_effects: [],"
+    + "gen_values: [],"
+    + "}";
     public static ArrayList<Entity> list = new ArrayList<Entity>();
+    public static ArrayList<String> themes = new ArrayList<String>();
+
+    public static Random rand = new Random();
 
     public static void create(int count) {
+        boolean requestsEnabled = false;
+
+        if (requestsEnabled) {
+            themes = RequestHandler.makeRequest("Provide a JSON in the format: {themes: []} where themes has 100 interesting words", 
+            "", 
+            0.1).getAsArrayList("themes");
+        }
 
         Entity newEnt = null;
         for (int i = 0; i < count; i++) {
