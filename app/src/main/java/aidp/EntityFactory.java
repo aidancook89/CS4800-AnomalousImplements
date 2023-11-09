14fafcc500a81dd1b062b31e60130492395c394e
aidancook89 <cookar4@appstate.edu>
2023-11-09 14:35:50 -0500

    Working on implementing OpenAI Calls into EntityFactory.

--- app/src/main/java/aidp/EntityFactory.java
+++ app/src/main/java/aidp/EntityFactory.java
@@ -2,6 +2,7 @@ package aidp;
 
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 
@@ -14,10 +15,55 @@ public class EntityFactory {
     + "modifiers: [],"
     + "potion_effects: [],"
     + "generic_effects: [],"
-    + "gen_values: [],"
+    //+ "gen_values: [],"
     + "}";
     public static ArrayList<Entity> list = new ArrayList<Entity>();
     public static ArrayList<String> themes = new ArrayList<String>();
+
+    public static ArrayList<String> modifiers = new ArrayList<String>(List.of(
+        "Silent",
+        "Glowing"
+    ));
+
+    public static ArrayList<String> potionEff = new ArrayList<String>(List.of(
+        "speed",
+        "slowness",
+        "haste",
+        "mining_fatigue",
+        "strength",
+        "instant_health",
+        "instant_damage",
+        "jump_boost",
+        "nausea",
+        "regeneration",
+        "resistance",
+        "fire_resistance",
+        "water_breathing",
+        "invisibility",
+        "blindness",
+        "night_vision",
+        "hunger",
+        "weakness",
+        "poison",
+        "wither",
+        "health_boost",
+        "absorption",
+        "saturation",
+        "glowing",
+        "levitation",
+        "luck",
+        "unluck",
+        "slow_falling"
+    ));
+
+    public static ArrayList <String> genericList = new ArrayList<String>(List.of(
+        "generic.knockback_resistance",
+        "generic.movement_speed",
+        "generic.attack_damage",
+        "generic.armor",
+        "generic.armor_toughness",
+        "generic.attack_knockback"
+    ));
 
     public static Random rand = new Random();
 
@@ -28,6 +74,29 @@ public class EntityFactory {
             themes = RequestHandler.makeRequest("Provide a JSON in the format: {themes: []} where themes has 100 interesting words", 
             "", 
             0.1).getAsArrayList("themes");
+        }
+
+        int modifiersCount = 0;
+        int potionEffectCount = 0;
+        int genEffectCount = 0;
+
+        for (int i = 0; i < count; i++) {
+            modifiersCount = rand.nextInt(2);
+            potionEffectCount = rand.nextInt(3);
+            genEffectCount = rand.nextInt(3);
+
+            String rules = String.format("{"
+            + "name: string,"
+            + "color: hexcode,"
+            + "modifiers: pick %d from [%s],"
+            + "potion_effects: pick %d from [%s],"
+            + "generic_effects: pick %d from [%s]"
+            + "}", modifiersCount, modifiers.toString(),
+            potionEffectCount, potionEff.toString(),
+            genEffectCount, genericList.toString());
+
+            if (requestsEnabled) createJsonAI(i, requestJson, rules, themes);
+            
         }
 
         Entity newEnt = null;
